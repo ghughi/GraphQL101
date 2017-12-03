9ce024a01a15fd8fe6c26b69c5a07e7016d3db22
Ajay Handa <ajay.handa@gmail.com>
2017-12-03 13:36:44 +0000

    Lecture 1: Coding Excercise: 2. Registering GraphQL with Express

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');                              // 2. Registering GraphQL with Express 
 const app = express();
+app.use('/graphql', expressGraphQL({                                            // 2. Registering GraphQL with Express
+    graphiql: true                                                              // 2. Registering GraphQL with Express
+}));   
 app.listen(process.env.PORT,process.env.IP, () => {
    console.log('Listening...') ;
 });
